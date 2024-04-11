d20aec738f0ba94eef82734b434abfcdf731cd0b
imbyeonghun <byeonghun6885@naver.com>
2024-04-11 13:36:55 +0900

    수정

--- src/main/java/kr/kh/team1/controller/CJYController.java
+++ src/main/java/kr/kh/team1/controller/CJYController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.kh.team1.model.vo.ProductVO;
 import kr.kh.team1.pagination.Criteria;
--- src/main/java/kr/kh/team1/controller/HomeController.java
+++ src/main/java/kr/kh/team1/controller/HomeController.java
@@ -1,7 +1,7 @@
 package kr.kh.team1.controller;
 
 import java.text.DateFormat;
-
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.team1.model.vo.TopGroupVO;
 import kr.kh.team1.service.MemberService;
 import kr.kh.team1.service.TopGroupService;
 
