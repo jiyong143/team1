8fe5e2551cb73a1cd4413ddf1dc59e6bef361e1b
imbyeonghun <byeonghun6885@naver.com>
2024-04-11 17:39:04 +0900

    1대1채팅
    Team1 lbh

--- src/main/java/kr/kh/team1/service/ChatServiceImp.java
+++ src/main/java/kr/kh/team1/service/ChatServiceImp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.kh.team1.dao.ChatDAO;
+import kr.kh.team1.model.dto.MessageDTO;
+import kr.kh.team1.model.vo.ChatMessageVO;
 import kr.kh.team1.model.vo.ChatRoomVO;
 
 @Service
@@ -14,11 +16,15 @@ public class ChatServiceImp implements ChatService {
 	@Autowired
 	ChatDAO chatDao;
 
+	private boolean CheckStr(String str) {
+		return str.length() != 0 && str != null;
+	}
+	
 	@Override
-	public ChatRoomVO getChatRoom(String user, int pr_num) {
-		if(user == null || pr_num == 0)
+	public ChatRoomVO getChatRoom(String me_id, int pr_num) {
+		if(me_id == null || pr_num == 0)
 			return null;
-		return chatDao.selectChatRoom(user, pr_num);
+		return chatDao.selectChatRoom(me_id, pr_num);
 	}
 
 	@Override
@@ -35,6 +41,20 @@ public class ChatServiceImp implements ChatService {
 			return null;
 		
 		return chatDao.selectChatRoomList(me_id);
+	}
+
+	@Override
+	public boolean insertChat(MessageDTO message) {
+		if(!CheckStr(message.getCm_me_id()) ||
+			!CheckStr(message.getCm_content()))
+			return false;
+		
+		return chatDao.insertChat(message);
+	}
+
+	@Override
+	public ArrayList<ChatMessageVO> getChatMessageList(int cr_num) {
+		return chatDao.selectChatMessgeList(cr_num);
 	}
 
 }
